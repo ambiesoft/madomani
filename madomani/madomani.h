a4501214bd997cf37de6cca5a9f6d9649f89fd3e
ambiesoft <ambiesoft.trueff@gmail.com>
2019-01-10 11:32:02 +0900

    Add license on the top of source files

--- madomani/madomani.h
+++ madomani/madomani.h
@@ -1,5 +1,26 @@
-// madomani.h : MADOMANI アプリケーションのメイン ヘッダー ファイルです。
+//Copyright (C) 2017 Ambiesoft All rights reserved.
 //
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions
+//are met:
+//1. Redistributions of source code must retain the above copyright
+//notice, this list of conditions and the following disclaimer.
+//2. Redistributions in binary form must reproduce the above copyright
+//notice, this list of conditions and the following disclaimer in the
+//documentation and/or other materials provided with the distribution.
+//
+//THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+//ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+//FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+//OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+//OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+//SUCH DAMAGE.
+
 
 #if !defined(AFX_MADOMANI_H__A0E2D342_078A_46FC_AA3A_EEBB849BA2BC__INCLUDED_)
 #define AFX_MADOMANI_H__A0E2D342_078A_46FC_AA3A_EEBB849BA2BC__INCLUDED_
@@ -12,12 +33,8 @@
 	#error include 'stdafx.h' before including this file for PCH
 #endif
 
-#include "resource.h"		// メイン シンボル
+#include "resource.h"
 
-/////////////////////////////////////////////////////////////////////////////
-// CMadomaniApp:
-// このクラスの動作の定義に関しては madomani.cpp ファイルを参照してください。
-//
 
 class CMadomaniApp : public CWinApp
 {
